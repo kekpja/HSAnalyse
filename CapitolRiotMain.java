45148ee1cd014b1771709a6fd0f95e693971d176
3scapeX <78601930+3scapeX@users.noreply.github.com>
2021-02-05 18:32:43 +0100

    Java project

---
+++ CapitolRiotMain.java
@@ -0,0 +1,17 @@
+package capitolriot;
+
+public class CapitolRiotMain {
+
+	public static void main(String[] args) {
+		
+		//Array: Days (as days)
+		
+		//Array: MeanTweets (double)
+		
+	
+		
+		
+		
+	}
+
+}
---
+++ src/capitolriot/CapitolRiotMain.java
@@ -0,0 +1,10 @@
+package capitolriot;
+
+public class CapitolRiotMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
