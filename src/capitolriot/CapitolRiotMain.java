2d2bae3a362ee19b7fb2ae12ae44f02f98522970
kekpja <56304716+kekpja@users.noreply.github.com>
2021-02-05 19:14:57 +0100

    Xchart

--- src/capitolriot/CapitolRiotMain.java
+++ src/capitolriot/CapitolRiotMain.java
@@ -1,10 +1,21 @@
 package capitolriot;
+import org.knowm.xchart.BitmapEncoder;
+import org.knowm.xchart.BitmapEncoder.BitmapFormat;
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
 
 public class CapitolRiotMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		double[] xData = new double[] { 0.0, 0.5, 2.0 };
+		double[] yData = new double[] { 2.0, 0.5, 0.0 };
 
+		// Create Chart
+		XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
+
+		// Show it
+		new SwingWrapper(chart).displayChart();
 	}
 
 }
---
+++ src/capitolriot/Test.java
@@ -0,0 +1,5 @@
+package capitolriot;
+
+public class Test {
+
+}
