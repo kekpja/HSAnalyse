abcf057407b76b78ec6ffded54c73b4efc1e9642
kekpja <56304716+kekpja@users.noreply.github.com>
2021-02-05 21:23:58 +0100

    Functions and Parametres

--- src/capitolriot/CapitolRiotMain.java
+++ src/capitolriot/CapitolRiotMain.java
@@ -1,21 +1,23 @@
 package capitolriot;
-import org.knowm.xchart.BitmapEncoder;
-import org.knowm.xchart.BitmapEncoder.BitmapFormat;
+import org.knowm.xchart.CategoryChart;
 import org.knowm.xchart.QuickChart;
 import org.knowm.xchart.SwingWrapper;
 import org.knowm.xchart.XYChart;
+import org.knowm.xchart.XYChartBuilder;
+import org.knowm.xchart.XYSeries;
+import org.knowm.xchart.XYSeries.XYSeriesRenderStyle;
+import org.knowm.xchart.style.Styler.LegendPosition;
+import org.knowm.xchart.style.markers.SeriesMarkers;
 
 public class CapitolRiotMain {
 
 	public static void main(String[] args) {
-		double[] xData = new double[] { 0.0, 0.5, 2.0 };
-		double[] yData = new double[] { 2.0, 0.5, 0.0 };
-
-		// Create Chart
-		XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
-
-		// Show it
-		new SwingWrapper(chart).displayChart();
+		
 	}
+	
+public void readJson(String path) {
+	
+	// TODO read data set in json format
+}
 
 }
--- src/capitolriot/GraphCreator.java
+++ src/capitolriot/GraphCreator.java
@@ -1,12 +1,71 @@
 package capitolriot;
 
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
+import org.knowm.xchart.XYChartBuilder;
+import org.knowm.xchart.XYSeries;
+import org.knowm.xchart.XYSeries.XYSeriesRenderStyle;
+import org.knowm.xchart.style.Styler.LegendPosition;
+import org.knowm.xchart.style.markers.SeriesMarkers;
+
 public class GraphCreator {
 	
-	public double[] avgSentimentDay(String date) {
+	public String[] dates;
+	public String[] hashtags;
+	
+	public double[] avgSentimentDay() {
 		return null;
 	}
 	
-	public void displayArray(){
-		
+	public double[] avgSentimentHashtag() {
+		return null;
+	}
+	
+	public int[] hashtagPerDay() {
+		return null;
+	}
+	
+	public int[] tweetPerDay() {
+		return null;
+	}
+	
+	public int[] tweetPerHashtag() {
+		return null;
+	}
+	
+	public void displaySentimentChart(double[] sentScores){
+		// Create Chart
+				XYChart chart = new XYChartBuilder().width(600).height(500).title("Gaussian Blobs").xAxisTitle("X").yAxisTitle("Y").build();
+
+				// Customize Chart
+				chart.getStyler().setDefaultSeriesRenderStyle(XYSeriesRenderStyle.Scatter);
+				chart.getStyler().setChartTitleVisible(false);
+				chart.getStyler().setLegendPosition(LegendPosition.InsideSW);
+				chart.getStyler().setMarkerSize(16);
+
+				// Series
+				chart.addSeries("a", new double[] { 0, 3, 5, 7, 9 }, new double[] { -3, 5, 9, 6, 5 });
+				XYSeries series = chart.addSeries("Gaussian Blob 2", new double[] { 0, 3, 5, 7, 9 }, new double[] { -3, 5, 9, 6, 5 });
+				series.setMarker(SeriesMarkers.DIAMOND);
+
+				new SwingWrapper(chart).displayChart();
+	}
+	
+	public void displayQuantityChart(int[] quantities){
+		// Create Chart
+				XYChart chart = new XYChartBuilder().width(600).height(500).title("Gaussian Blobs").xAxisTitle("X").yAxisTitle("Y").build();
+
+				// Customize Chart
+				chart.getStyler().setDefaultSeriesRenderStyle(XYSeriesRenderStyle.Scatter);
+				chart.getStyler().setChartTitleVisible(false);
+				chart.getStyler().setLegendPosition(LegendPosition.InsideSW);
+				chart.getStyler().setMarkerSize(16);
+
+				// Series
+				chart.addSeries("a", new double[] { 0, 3, 5, 7, 9 }, new double[] { -3, 5, 9, 6, 5 });
+				XYSeries series = chart.addSeries("Gaussian Blob 2", new double[] { 0, 3, 5, 7, 9 }, new double[] { -3, 5, 9, 6, 5 });
+				series.setMarker(SeriesMarkers.DIAMOND);
+
+				new SwingWrapper(chart).displayChart();
 	}
 }
